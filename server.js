17552e42242e7f79a5013b6e6d89102842b1d9f8
thanhnm <thanhbka@yahoo.com>
2019-09-28 15:51:01 +0700

    init

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const history = require('connect-history-api-fallback')
 const app = express()
 app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')))
-let port = process.env.PORT || 5000
+let port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
